8c4f7e04bcef7bec6e6273e1d922c50a47b74e8d
Jason Murphy <jmurphy@jasonmsiws.local>
2019-05-29 14:38:57 -0500

    add function file

---
+++ lambdata_biovir3/functions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+#import numpy as np
+#import pandas as pd
+
+class Cleaner:
+    df = pd.DataFrame()
+
+    def __init__(self, df):
+        self.df=df
+
+    def dropna(self):
+        self.df.dropna()
+
+    def retdf(self):
+        return self.df
+
+    def desc(self):
+        return self.df.describe()
