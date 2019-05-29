1a9dd3a42e8ca6e5e68f6d7f0a846e362af37355
Jason Murphy <jmurphy@jasonmsiws.local>
2019-05-28 22:42:38 -0500

    fixed __init__

--- lambdata_biovir3/__init__.py
+++ lambdata_biovir3/__init__.py
@@ -5,4 +5,6 @@ Biovir3 Helper DS Functions
 import pandas as pd
 import numpy as np
 
+from . import example_module
+
 TEST = pd.DataFrame(np.ones(10))
---
+++ setup.py
@@ -0,0 +1,31 @@
+""" lambdata - Data Science Helper Functions
+"""
+
+import setuptools
+
+REQUIRED = [
+    'numpy',
+    'pandas'
+]
+
+with open("README.md",'r') as fh:
+     LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name = 'lambdata-biovir3',
+    version = '0.0.0.0.1',
+    author = 'biovir3',
+    author_email = 'biovir3@gmail.com',
+    description = 'DS Helper Functions',
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type = 'text/markdown',
+    url='https://github.com/biovir3/lambdata',
+    packages = setuptools.find_packages(),
+    python_requires = '>=3.5',
+    install_requires = REQUIRED,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
